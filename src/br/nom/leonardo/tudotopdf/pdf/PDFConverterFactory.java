0492e004f426a5d7da376bfbe38d4f54731c6232
Leonardo Cardoso (Ubuntu-Devel-64) <leonardo.f.cardoso@gmail.com>
2015-02-22 22:38:53 -0300

    Added TXT and RTF support

--- src/br/nom/leonardo/tudotopdf/pdf/PDFConverterFactory.java
+++ src/br/nom/leonardo/tudotopdf/pdf/PDFConverterFactory.java
@@ -13,6 +13,10 @@ public class PDFConverterFactory {
 	private static final String DOC = "application/msword";
 	private static final String XLS = "application/vnd.ms-excel";
 	private static final String PPT = "application/vnd.ms-powerpoint";
+	
+	private static final String RTF = "application/rtf";
+	
+	private static final String TXT = "text/plain";
 
 	private static final String DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
 	private static final String XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
@@ -41,7 +45,7 @@ public class PDFConverterFactory {
 		}
 
 		if ("JOD".equals(strategy)
-				&& Arrays.asList(new String[] { DOC, XLS, PPT, DOCX, PPTX, XLSX, ODT, ODS, ODP }).contains(contentType)) {
+				&& Arrays.asList(new String[] { DOC, XLS, PPT, RTF, TXT, DOCX, PPTX, XLSX, ODT, ODS, ODP }).contains(contentType)) {
 			return new JODConverter();
 		}
 
@@ -50,11 +54,11 @@ public class PDFConverterFactory {
 		}
 
 		if ("OfficeToPDF".equals(strategy)
-				&& Arrays.asList(new String[] { DOC, XLS, PPT, DOCX, XLSX, PPTX }).contains(contentType)) {
+				&& Arrays.asList(new String[] { DOC, XLS, PPT, RTF, DOCX, XLSX, PPTX }).contains(contentType)) {
 			return new OfficeToPDFConverter();
 		}
 
-		if ("AsposeWords".equals(strategy) && Arrays.asList(new String[] { DOC, DOCX, ODT }).contains(contentType)) {
+		if ("AsposeWords".equals(strategy) && Arrays.asList(new String[] { DOC, DOCX, ODT, RTF, TXT }).contains(contentType)) {
 			return new AsposeWordsConverter();
 		}
 
